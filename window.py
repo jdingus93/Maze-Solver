b00e5c1fed47f179b3746735a9437b645f0e8735
Sponge <spongedingus@gmail.com>
2024-10-24 13:43:04 -0500

    Added draw_line function

--- window.py
+++ window.py
@@ -17,6 +17,9 @@ class Window:
         self.__root.update_idletasks()
         self.__root.update()
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
     def wait_for_close(self):
         self.__running = True
         while self.__running:
@@ -24,7 +27,38 @@ class Window:
     
     def close(self):
         self.__running = False
-        
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x, self.point1.y,
+            self.point2.x, self.point2.y,
+            fill = fill_color,
+            width=2
+        )
+
 if __name__ == "__main__":
     win = Window(800, 600)
+
+    point1 = Point(100, 200)
+    point2 = Point(300, 400)
+    point3 = Point(800, 600)
+    point4 = Point(475, 322)
+    
+    line1 = Line(point1, point2)
+    line2 = Line(point3, point4)
+
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "blue")
+
     win.wait_for_close()
+
