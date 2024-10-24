29579869cac8aa9dab75e663e0bf3f193cf15739
Sponge <spongedingus@gmail.com>
2024-10-24 14:40:22 -0500

    Creating cell class

--- window.py
+++ window.py
@@ -46,19 +46,50 @@ class Line:
             width=2
         )
 
+class Cell:
+    def __init__(self, x1, x2, y1, y2, win):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+    def draw(self):
+        if self.has_left_wall:
+            start = Point(self._x1, self._y1)
+            end = Point(self._x1, self._y2)
+            line = Line(start, end)
+            self._win.draw_line(line, "black")
+        if self.has_right_wall:
+            start = Point(self._x2, self._y1)
+            end = Point(self._x2, self._y2)
+            line = Line(start, end)
+            self._win.draw_line(line, "black")
+        if self.has_top_wall:
+            start = Point(self._x1, self._y1)
+            end = Point(self._x2, self._y1)
+            line = Line(start, end)
+            self._win.draw_line(line, "black")
+        if self.has_bottom_wall:
+            start = Point(self._x1, self._y2)
+            end = Point(self._x2, self._y2)
+            line = Line(start, end)
+            self._win.draw_line(line, "black")
+
 if __name__ == "__main__":
-    win = Window(800, 600)
+    win = Window(1920, 1440)
 
-    point1 = Point(100, 200)
-    point2 = Point(300, 400)
-    point3 = Point(800, 600)
-    point4 = Point(475, 322)
-    
-    line1 = Line(point1, point2)
-    line2 = Line(point3, point4)
+    cell1 = Cell(50, 100, 50, 100, win)
+    cell1.draw()
 
-    win.draw_line(line1, "black")
-    win.draw_line(line2, "blue")
+    cell2 = Cell(150, 200, 50, 100, win)
+    cell2.has_right_wall= False
+    cell2.has_bottom_wall= False
+    cell2.draw()
 
     win.wait_for_close()
 
